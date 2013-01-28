8a4a7f9186f4aa94a435cbca03047623e7a5c9fb
Eric Jimenez <eric.andres.jimenez@gmail.com>
2013-01-28 10:41:33 -0500

    Document empty functions that require implementation

--- A1/comments.js
+++ A1/comments.js
@@ -1,24 +1,37 @@
 //  Global variable comments contains all comment functionality
 var comments = {
+
+	// I don't know what are the parameters of this function, but they will stem from the json data
+	// Return a comment html string
+	create: function(){ 
+		// Magic with json data and strings...
+	},
+
+	// Place comment created from data on the corresponding node
+	render: function(data, node_id){
+		// Use create for the html string and jquery to append to the DOM
+	},
     
     // Show the comment section for topic with the matching id
     show: function(section){
         
         // Get the id of the topic containing the comment section
-        topic_id = $(section).parent().parent().attr("id");
+        node_id = $(section).parent().parent().attr("id");
         
-        // Debug:
-        console.log(topic_id);
+        // DEBUG: Proof that clicking on the topic returns the id of the topic
+        console.log(node_id);
         
-        var url = '/topic?id=' + topic_id;
+        var url = '/topic?id=' + node_id;
         
 //        // Get comment data from server (will cause error if no server is present)
 //        $.getJSON(url, function(data){
 //            
-//            // Populate DOM with comment data that came from server
+//            // Populate DOM with comment data that came from server.
+//			  // Look at each comment attached to the origin node (the topic)
 //            $.each(data, function() {
-//                
-//                // Append stuff to the DOM
+//
+//                // Append comments to the comment section of the topic
+//				  // Recursively check whether or not that comment has a comment attached to it
 //                
 //            });
 //        });
