d208534607a630fbfeed1e8d4373c974bf8e9072
daphnei <daphnepp@gmail.com>
2013-02-06 14:16:09 -0500

    Server sends topic list to client, and allows client to submit new topics.

---
+++ A1/data.js
@@ -0,0 +1,35 @@
+var nodes = new Array();
+
+function insertComment(content, root) {
+	var node = {};
+	
+	node.type = "comment";
+	node.content = content;
+	node.vote_count = 0;
+	node.id = nodes.length;
+	node.children_ids = new Array();
+	node.root_id = root;
+
+	nodes.push(node);
+}
+/**
+ * Inserts a node representing a topic
+ * 
+ * returns the new node
+ */
+function insertTopic(description, link) {
+	var node = {};
+	
+	node.type = "topic";
+	node.content = description;
+	node.vote_count = 0;
+	node.id = nodes.length;
+	node.children_ids = new Array();
+	node.link = link
+	
+	nodes.push(node);
+	return node;
+}
+
+exports.insertTopic = insertTopic;
+exports.nodes = nodes;
--- A1/server.js
+++ A1/server.js
@@ -1,93 +1,16 @@
-http = require('http');
-fs = require('fs');
-path = require('path');
-url = require('url');
+var http = require("http");
+var url = require("url");
 
-MIME_TYPES = {
-        '.html': 'text/html',
-        '.css': 'text/css',
-        '.js': 'text/javascript',
-        '.txt': 'text/plain'
-};
+function start(route, handle) {
+  function onRequest(request, response) {
+    var pathname = url.parse(request.url).pathname;
+    console.log("Request for " + pathname + " received.");
+    route(handle, pathname, response, request);
+  }
 
-var nodes = new Array();
-
-http.createServer(onRequest).listen(1234);
-
-function onRequest (req, res) {
-	if (req.url == '/' || req.url == '/topic') {
-		// home / get list of topics
-		console.log('Hit homepage.');
-		res.writeHead(200, {'Content-Type' : MIME_TYPES['.html']});
-		var buffer = fs.readFile('index.html',
-			function(err, data) { serveFile(err, data, res); } );
-	}
-	else {
-		// For everything else (CSS file, scripts, etc.) assume it's asking for a static file.
-		console.log('Requested file ' + req.url);
-		
-		var urlpath = url.parse(req.url).pathname;
-		res.writeHead(200, {'Content-Type' : MIME_TYPES[path.extname(urlpath)]});
-
-		var filename = path.relative("/", urlpath);
-		var buffer = fs.readFile(filename, function(err, data) { serveFile(err, data, res); });
-	}
-	
-	// TODO:
-
-	// submit topic
-	
-	// submit comment
-	
-	// specific topic / get list of comments
-	
-	// upvote comment	
+  http.createServer(onRequest).listen(1234);
+  console.log("Server has started.");
 }
 
-function serveFile(err, data, res) {
-	if (err) {
-		// Overwrite the header with a 404 error.
-		res.writeHead(404, {'Content-Type' : MIME_TYPES['.txt']});
-		res.end("404: file not found.");
-	}
-	else {
-		res.end(data);
-	}
-};
+exports.start = start;
 
-function insertComment(type, content, root) {
-	var node = {};
-	
-	node.type = type;
-	node.content = content;
-	node.vote_count = 0;
-	node.id = nodes.length;
-	node.children_ids = new Array();
-	node.root_id = root;
-
-	nodes.push(node);
-}
-
-function insertTopic(type, description, link) {
-	var node = {};
-	
-	node.type = type;
-	node.content = description;
-	node.vote_count = 0;
-	node.id = nodes.length;
-	node.children_ids = new Array();
-	node.link = link
-	
-	nodes.push(node);
-}
-
-function upvote(id) {
-	var comment = nodes[id];
-	if (comment.type !=  "comment") {
-		console.log("Tried to upvote a non-comment node.");
-		return false;
-	}
-	comment.vote_count++;
-	var root = nodes[comment.root_id];
-	root.vote_count++;
-}
