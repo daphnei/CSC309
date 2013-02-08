9237a035d0d9bf712ff9996c4a1ae72bdbf5ebb6
daphnei <daphnepp@gmail.com>
2013-02-07 21:23:01 -0500

    Some small changes and refractoring of comments code

--- A1/index.js
+++ A1/index.js
@@ -8,9 +8,9 @@ handle["/"] = requestHandlers.start; //the initial page with a list of topics
 handle["/index"] = requestHandlers.start; //the initial page with a list of topics
 handle["/topic"] = requestHandlers.getNodeFromIndex; // sends the topic node for the given id
 handle["/topics"] = requestHandlers.getTopics; //the initial page with a list of topics
-handle["/topics/submit"] = requestHandlers.submitTopic; //sending in the filled out topic or comment form
+handle["/topics/submit"] = requestHandlers.submitTopic; //sending in the filled out topic  form
 handle["/upvote"] = requestHandlers.upvote; //upvote a comment
-handle["/reply"] = requestHandlers.submitComment; //submit a comment
+handle["/reply"] = requestHandlers.submitComment;  //sending the filled out comments form
 handle["/comments"] = requestHandlers.getComments; //get the comments for a particular topic
 
 /* various assets */
@@ -18,5 +18,6 @@ handle["/style.css"] = requestHandlers.getAsset; //the CSS for the site
 handle["/comments.js"] = requestHandlers.getAsset; //client-side javascript
 handle["/jquery.js"] = requestHandlers.getAsset; //client-side javascript
 handle["/topics.js"] = requestHandlers.getAsset; //client-side javascript
+handle["/favicon.ico"] = requestHandlers.getAsset; //client-side javascript
 
 server.start(router.route, handle);
